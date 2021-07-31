410f88affbd8fa1c86e5026f0408869fb34d3bc6
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-07-31 08:42:08 -0300

    X01BD: add powerhint from common Signed-off-by: allansouzadk <allan.pereira.araujo02@gmail.com> Signed-off-by: allansouzadk <allansouzadk@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -27,6 +27,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# Power configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Wifi
 PRODUCT_PACKAGES += \
     X01BDWifiOverlay
